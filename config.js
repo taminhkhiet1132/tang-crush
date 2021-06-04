f08ff6de888a84f6cf3dffc424b7a660ab8f5d2e
Minh-Khiết <85308660+taminhkhiet1132@users.noreply.github.com>
2021-06-05 02:27:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Linh à ',
+    introTitle: 'My à ',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao My cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'ấn vô đây nè Linh',
-    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên :V',
+    btnIntro: 'ấn vô đây nè My',
+    title: 'Phải chăng My đã yêu anh ngay từ cái nhìn đầu tiên :V',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh iu nhìu <33',
+    btnYes: 'Vẫn cứ là thích anh nhìu <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu hehe <33333333',
+    reply: 'Khiết à anh đẹp trai lắm em yêu anh  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'đêm nay 1h anh anh call nhé công chúa.',
+    messDesc: 'có gì anh đi học về in búc cho em nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/minhkhiet2003' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100029550452165' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
