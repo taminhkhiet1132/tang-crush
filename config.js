44e4364074fc2e527d9bd27695e6068d22d377fc
Minh-Khiết <85308660+taminhkhiet1132@users.noreply.github.com>
2021-06-04 13:54:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Linh af ',
+    introTitle: 'Linh à ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hehe',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên :V',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên :V',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh iu <33',
+    btnYes: 'Vẫn cứ là thích anh iu nhìu <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
