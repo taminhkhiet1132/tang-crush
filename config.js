8e2fc69dab5c1db2064c0ff35c49cc501a7d1b28
Minh-Khiết <85308660+taminhkhiet1132@users.noreply.github.com>
2021-07-25 23:06:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
   
 const CONFIG = {
-    introTitle: 'Huyền à!',
+    introTitle: 'Lan Phương à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'ấn zô đây nè',
-    title: 'Phải chăng Huyền đã yêu anh từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Huyền đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Phương đã yêu anh từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Phương đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh nhé <3',
-    reply: 'KHiết ơi anh đẹp trai lắm! em yêu anh <33333333',
+    reply: 'Khiết ơi anh đẹp trai lắm! em yêu anh <33333333',
     mess: 'Anh biết mà:33. Yêu em nhiều nhiều 😘😘',
     messDesc: 'em đồng ý làm người yêu của anh nhé <3.',
     btnAccept: 'Okiiiii lun <3',
